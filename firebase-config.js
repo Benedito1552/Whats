89e9b0c564b56f2e723f21e39e76be1cf059907b
Benedito1552 <beneditojrtimonha@gmail.com>
2025-10-18 14:09:20 -0300

    Refactor Firebase configuration and initialization
    Updated Firebase imports and initialization method.

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,17 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC0dUhymi72gkug-oo0-xqqBEtgvlIgvY0",
-    authDomain: "cursos-6f950.firebaseapp.com",
-    projectId: "cursos-6f950",
-    storageBucket: "cursos-6f950.firebasestorage.app",
-    messagingSenderId: "146131122545",
-    appId: "1:146131122545:web:043f8207f457ad5b7f5ed0"
-  };
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+  apiKey: "AIzaSyC0dUhymi72gkug-oo0-xqqBEtgvlIgvY0",
+  authDomain: "cursos-6f950.firebaseapp.com",
+  projectId: "cursos-6f950",
+  storageBucket: "cursos-6f950.firebasestorage.app",
+  messagingSenderId: "146131122545",
+  appId: "1:146131122545:web:043f8207f457ad5b7f5ed0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
